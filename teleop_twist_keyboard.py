eebde36b612ceab094fc77527d7e1700612b5e2d
grovehilldr <112022928+grovehilldr@users.noreply.github.com>
2023-08-29 03:38:05 -0400

    Update teleop_twist_keyboard.py

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -28,69 +28,6 @@ import numpy as np
 from scipy.special import comb
 from geometry_msgs.msg import TransformStamped, PoseStamped
 
-def create_si_to_uni_dynamics(linear_velocity_gain=1, angular_velocity_limit=np.pi):
-	""" Returns a function mapping from single-integrator to unicycle dynamics with angular velocity magnitude restrictions.
-
-        linear_velocity_gain: Gain for unicycle linear velocity
-        angular_velocity_limit: Limit for angular velocity (i.e., |w| < angular_velocity_limit)
-
-        -> function
-    """
-
-	# Check user input types
-	assert isinstance(linear_velocity_gain, (int,
-											 float)), "In the function create_si_to_uni_dynamics, the linear velocity gain (linear_velocity_gain) must be an integer or float. Recieved type %r." % type(
-		linear_velocity_gain).__name__
-	assert isinstance(angular_velocity_limit, (int,
-											   float)), "In the function create_si_to_uni_dynamics, the angular velocity limit (angular_velocity_limit) must be an integer or float. Recieved type %r." % type(
-		angular_velocity_limit).__name__
-
-	# Check user input ranges/sizes
-	assert linear_velocity_gain > 0, "In the function create_si_to_uni_dynamics, the linear velocity gain (linear_velocity_gain) must be positive. Recieved %r." % linear_velocity_gain
-	assert angular_velocity_limit >= 0, "In the function create_si_to_uni_dynamics, the angular velocity limit (angular_velocity_limit) must not be negative. Recieved %r." % angular_velocity_limit
-
-	def si_to_uni_dyn(dxi, poses):
-		"""A mapping from single-integrator to unicycle dynamics.
-
-        dxi: 2xN numpy array with single-integrator control inputs
-        poses: 2xN numpy array with single-integrator poses
-
-        -> 2xN numpy array of unicycle control inputs
-        """
-
-		# Check user input types
-		assert isinstance(dxi,
-						  np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the single integrator velocity inputs (dxi) must be a numpy array. Recieved type %r." % type(
-			dxi).__name__
-		assert isinstance(poses,
-						  np.ndarray), "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the current robot poses (poses) must be a numpy array. Recieved type %r." % type(
-			poses).__name__
-
-		# Check user input ranges/sizes
-		assert dxi.shape[
-				   0] == 2, "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the dimension of the single integrator velocity inputs (dxi) must be 2 ([x_dot;y_dot]). Recieved dimension %r." % \
-							dxi.shape[0]
-		assert poses.shape[
-				   0] == 3, "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the dimension of the current pose of each robot must be 3 ([x;y;theta]). Recieved dimension %r." % \
-							poses.shape[0]
-		assert dxi.shape[1] == poses.shape[
-			1], "In the si_to_uni_dyn function created by the create_si_to_uni_dynamics function, the number of single integrator velocity inputs must be equal to the number of current robot poses. Recieved a single integrator velocity input array of size %r x %r and current pose array of size %r x %r." % (
-		dxi.shape[0], dxi.shape[1], poses.shape[0], poses.shape[1])
-
-		M, N = np.shape(dxi)
-
-		a = np.cos(poses[2, :])
-		b = np.sin(poses[2, :])
-
-		dxu = np.zeros((2, N))
-		dxu[0, :] = linear_velocity_gain * (a * dxi[0, :] + b * dxi[1, :])
-		dxu[1, :] = angular_velocity_limit * np.arctan2(-b * dxi[0, :] + a * dxi[1, :], dxu[0, :]) / (np.pi / 2)
-
-		return dxu
-
-	return si_to_uni_dyn
-
-
 def create_si_to_uni_mapping(projection_distance=0.05, angular_velocity_limit=np.pi):
 	"""Creates two functions for mapping from single integrator dynamics to
     unicycle dynamics and unicycle states to single integrator states.
